4187e1a16d3ff6f926b909f430fa045b5f3f1f75
Rasul <rasuldev@gmail.com>
2025-11-27 00:10:03 +0300

    Legendre Sobolev

---
+++ mathlib/Polynomials/LegendreSobolev.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Math;
+
+namespace mathlib.Polynomials
+{
+    public static class LegendreSobolev
+    {
+        /// <summary>
+        /// Legendre - Sobolev polynomial with $r=1$.
+        /// </summary>
+        /// <param name="k">k >= 0</param>
+        /// <returns></returns>
+        public static Func<double, double> Get(int k)
+        {
+            if (k == 0)
+                return x => 1;
+
+            return x => (1-x*x);
+        }
+    }
+}
