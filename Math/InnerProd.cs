989ef6317e6d2a6551eace1ef86b3fd49d82a667
Rasul <rasuldev@gmail.com>
2025-11-30 02:03:12 +0300

    Legendre Sobolev inner product

--- Math/InnerProd.cs
+++ Math/InnerProd.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using mathlib.Symbolic;
+using static System.Math;
 
 namespace mathlib
 {
@@ -20,6 +21,59 @@ namespace mathlib
         public static Integral LebesgueLaguerre(Func<double, double> f, Func<double, double> g)
         {
             return WeightedLebesgue(f, g, x => Math.Exp(-x));
+        }
+
+        /// <summary>
+        /// Computes the Sobolev inner product ⟨f,g⟩ = f(a)g(a) + ∫_a^b f'(x)g'(x)(1-x)^α(1+x)^β dx
+        /// </summary>
+        /// <param name="f">Function f</param>
+        /// <param name="g">Function g</param>
+        /// <param name="a">Lower bound</param>
+        /// <param name="b">Upper bound</param>
+        /// <param name="alpha">Alpha parameter for weight</param>
+        /// <param name="beta">Beta parameter for weight</param>
+        /// <param name="h">Step size for numerical differentiation (default 1e-5)</param>
+        /// <param name="nodesCount">Number of nodes for numerical integration (default 1000)</param>
+        /// <returns>The value of the inner product</returns>
+        public static double SobolevJacobi(Func<double, double> f, Func<double, double> g, double a, double b, double alpha, double beta, double h = 1e-5, int nodesCount = 1000)
+        {
+            // Boundary term
+            double boundary = f(a) * g(a);
+
+            // Derivative functions
+            Func<double, double> fPrime = x => (f(x + h) - f(x - h)) / (2 * h);
+            Func<double, double> gPrime = x => (g(x + h) - g(x - h)) / (2 * h);
+
+            // Integrand: f'(x)g'(x)(1-x)^α(1+x)^β
+            Func<double, double> integrand = x => fPrime(x) * gPrime(x) * Pow(1 - x, alpha) * Pow(1 + x, beta);
+
+            // Numerical integration
+            double integral = Integrals.Trapezoid(integrand, a, b, nodesCount);
+
+            return boundary + integral;
+        }
+
+        public static double SobolevJacobi(Func<double, double> f, Func<double, double> g, double alpha, double beta, double h = 1e-5, int nodesCount = 1000)
+        {
+            return SobolevJacobi(f, g, -1, 1, alpha, beta, h, nodesCount);
+        }
+
+        public static double SobolevLegendre(Func<double, double> f, Func<double, double> g, double h = 1e-5, int nodesCount = 1000)
+        {
+            // Boundary term
+            double boundary = f(-1) * g(-1);
+
+            // Derivative functions
+            Func<double, double> fPrime = x => (f(x + h) - f(x - h)) / (2 * h);
+            Func<double, double> gPrime = x => (g(x + h) - g(x - h)) / (2 * h);
+
+            // Integrand: f'(x)g'(x)(1-x)^α(1+x)^β
+            Func<double, double> integrand = x => fPrime(x) * gPrime(x);
+
+            // Numerical integration
+            double integral = Integrals.Trapezoid(integrand, -1, 1, nodesCount);
+
+            return boundary + integral;
         }
     }
 
---
+++ Tests/Polynomials/LegendreSobolevTests.cs
@@ -0,0 +1,36 @@
+using mathlib;
+using mathlib.Polynomials;
+using NUnit.Framework;
+
+namespace Tests.Polynomials
+{
+    [TestFixture]
+    public class LegendreSobolevTests
+    {
+        [Test]
+        public void OrthonormalityTest()
+        {
+            int maxN = 5; // Test up to degree 4
+            double tolerance = 1e-3; // Tolerance for numerical integration
+
+            for (int i = 0; i < maxN; i++)
+            {
+                var p_i = LegendreSobolev.Get(i);
+                for (int j = 0; j < maxN; j++)
+                {
+                    var p_j = LegendreSobolev.Get(j);
+                    double innerProduct = InnerProd.SobolevLegendre(p_i, p_j);
+
+                    if (i == j)
+                    {
+                        Assert.That(innerProduct, Is.EqualTo(1.0).Within(tolerance), $"Norm of P_{i} should be 1");
+                    }
+                    else
+                    {
+                        Assert.That(innerProduct, Is.EqualTo(0.0).Within(tolerance), $"Inner product of P_{i} and P_{j} should be 0");
+                    }
+                }
+            }
+        }
+    }
+}
